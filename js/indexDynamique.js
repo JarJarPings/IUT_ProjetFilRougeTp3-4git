fb6895d1f4f3b9a483e450d63c8cf3a2274c6fb0
Quentin.Forestier <quentin.forestier@univ-grenoble-alpes.fr>
2022-11-07 11:54:03 +0100

    maj v0.03 07/11/22 11:53

--- js/indexDynamique.js
+++ js/indexDynamique.js
@@ -22,76 +22,100 @@ const ancreVideo = document.getElementById('ancreVideo');
 const ancreContact = document.getElementById('ancreContact');
 const ancreAnimaux = document.getElementById('ancreAnimaux');
 
-//ancreAcceuill.style.visibility = "hidden";
-
-let indexMenuValue;
+//Récup ID MainTitle
+let mainTitle = document.getElementById('MainTitle');
 
 //Par différent events onClick on indique quels éléments DOM s'affiche ou non
 
-
 //ANCRE ACCEUIL
 ancreAcceuill.onclick = function (){
+
+    mainTitle.innerText = "Acceuil";
+
     mainSectAcceuil.style.display = "block";
     listInfosPerso.style.display = "none";
     divPageAudio.style.display = "none";
     divPageVideo.style.display = "none";
     formPageContact.style.display = "none";
     divTableAnimaux.style.display = "none";
+
+
     //indexMenuValue = 1;
     //console.log(indexMenuValue);
 
 };
 
-console.log(indexMenuValue);
+
 
 
 //ANCRE PERSO
 ancrePerso.onclick = function (){
-    //indexMenuValue = 2;
+
+    mainTitle.innerText = "Mes infos persos !";
+
     listInfosPerso.style.display = "block";
     mainSectAcceuil.style.display = "none";
     divPageAudio.style.display = "none";
     divPageVideo.style.display = "none";
     formPageContact.style.display = "none";
     divTableAnimaux.style.display = "none";
+
+
     //console.log(indexMenuValue);
 
 };
 
 //ANCRE AUDIO
 ancreAudio.onclick = function (){
+
+    mainTitle.innerText = "Visite audio";
+
     divPageAudio.style.display = "block";
     mainSectAcceuil.style.display = "none";
     listInfosPerso.style.display = "none";
     divPageVideo.style.display = "none";
     formPageContact.style.display = "none";
     divTableAnimaux.style.display = "none";
+
+
 };
 
 //ANCRE VIDEO
 ancreVideo.onclick = function (){
+
+    mainTitle.innerText = "Visite vidéo";
+
     divPageVideo.style.display = "block";
     divPageAudio.style.display = "none";
     mainSectAcceuil.style.display = "none";
     listInfosPerso.style.display = "none";
     formPageContact.style.display = "none";
     divTableAnimaux.style.display = "none";
+
+
 };
 
 
 //ANCRE CONTACT
 ancreContact.onclick = function (){
+
+    mainTitle.innerText = "Contact";
+
     formPageContact.style.display = "block";
     divPageVideo.style.display = "none";
     divPageAudio.style.display = "none";
     mainSectAcceuil.style.display = "none";
     listInfosPerso.style.display = "none";
     divTableAnimaux.style.display = "none";
+
 };
 
 
 //ANCRE ANIMAUX
 ancreAnimaux.onclick = function (){
+
+    mainTitle.innerText = "Nos animaux";
+
     divTableAnimaux.style.display = "block";
     formPageContact.style.display = "none";
     divPageVideo.style.display = "none";
@@ -99,35 +123,3 @@ ancreAnimaux.onclick = function (){
     mainSectAcceuil.style.display = "none";
     listInfosPerso.style.display = "none";
 };
-
-/*
-if (indexMenuValue !== 1){
-    mainSectAcceuil.style.display = "none";
-}else if (indexMenuValue == 1){
-    mainSectAcceuil.style.display = "block";
-};
-*/
-
-/*
-if (indexMenuValue == 1){
-    console.log("index = 1");
-    mainSectAcceuil.style.display = "block";
-}else if (indexMenuValue == 2){
-    console.log("index = 2");
-    listInfosPerso.style.display = "block";
-};
-
-
-
-function calculIndex (valIndx){
-    if (valIndx == 1){
-        console.log("index = 1");
-        mainSectAcceuil.style.display = "block";
-    }else if (valIndx == 2){
-        console.log("index = 2");
-        listInfosPerso.style.display = "block";
-    };
-}
-calculIndex(indexMenuValue);
-
- */
